1633f6fddd3f163cc4aa688c708cd71b1ce66a0b
naz34 <durmussehrinaz@gmail.com>
2025-01-06 11:34:45 +0300

    update TestRunner

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,14 +6,13 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "C:/Users/sdurmus/IdeaProjec/TrendyolExample/src/test/resources/features", // Feature dosyalarının yolu
+        features = "src/test/resources/features", // Feature dosyalarının yolu
         glue = {"stepDefinations"},       // Step Definitions ve gerekli class'ların yolu
         plugin = {
                 "pretty",                           // Konsol çıktısını daha okunabilir hale getirir
                 "html:target/cucumber-reports.html", // HTML raporu oluşturur
                 "json:target/cucumber-reports.json",
-                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
-                //"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"// Allure raporu için
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"// Allure raporu için
         },
         monochrome = true,   // Konsol çıktısını daha okunabilir hale getirir
         dryRun = false
